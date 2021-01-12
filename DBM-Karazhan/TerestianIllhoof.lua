f100887d09784724bddac269aa5e85d241953549
notJunior <rayzeed1@gmail.com>
2021-01-12 22:59:31 +0000

    Illhoof: Additional heroic support and fixes for Curse not showing destination name.

--- DBM-Karazhan/TerestianIllhoof.lua
+++ DBM-Karazhan/TerestianIllhoof.lua
@@ -26,6 +26,7 @@ local warningImp		= mod:NewSpellAnnounce(30066, 3)
 local warningSacSoon	= mod:NewSoonAnnounce(85190, 3)
 local warningSacrifice	= mod:NewTargetAnnounce(85190, 4)
 local warnCurse			= mod:NewTargetAnnounce(85275, 3)
+local warnLink			= mod:NewTargetAnnounce(85277, 3)
 
 local specWarnSacrifice	= mod:NewSpecialWarningYou(85190)
 local specWarnCurse		= mod:NewSpecialWarningYou(85275)
@@ -35,6 +36,8 @@ local timerSacrifice	= mod:NewTargetTimer(30, 85190)
 local timerSacrificeCD	= mod:NewNextTimer(43, 85190)
 local timerCurse		= mod:NewTargetTimer(15, 85275)
 local timerCurseCD		= mod:NewNextTimer(40, 85275)
+local timerLink			= mod:NewTargetTimer(10, 85277)
+local timerAmplifyCD	= mod:NewNextTimer(9, 85289)
 
 local berserkTimer		= mod:NewBerserkTimer(600)
 
@@ -63,11 +66,14 @@ function mod:SPELL_AURA_APPLIED(args)
 		warningImpSoon:Schedule(26)
 	elseif args:IsSpellID(85275) then
 		warnCurse:Show(args.destName)
-		timerCurse:Start()
+		timerCurse:Start(args.destName)
 		timerCurseCD:Start()
 		if args:IsPlayer() then
 			specWarnCurse:Show()
 		end
+	elseif args:IsSpellID(85277) then
+		warnLink:Show(args.destName)
+		timerLink:Start(args.destName)
 	end
 end
 
@@ -82,6 +88,8 @@ function mod:SPELL_CAST_SUCCESS(args)
 		warningImpSoon:Cancel()
 		warningImp:Show()
 		DBM.BossHealth:AddBoss(17229, L.Kilrek)
+	elseif args:IsSpellID(85289) then
+		timerAmplifyCD:Start()
 	end
 end
 
@@ -89,6 +97,8 @@ function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 17229 then--Kil'rek
 		DBM.BossHealth:RemoveBoss(cid)
+		timerLink:Cancel()
+		timerAmplifyCD:Cancel()
 	elseif cid == 17248 then--Demon Chains
 		DBM.BossHealth:RemoveBoss(cid)
 	end
