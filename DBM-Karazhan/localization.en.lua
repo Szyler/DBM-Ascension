40fde871f92fbe2b97eb91244b91b4a79685de93
Szyler <Szyler@Szyler.com>
2021-05-16 20:34:49 +0200

    LUA error fixes

--- DBM-Karazhan/localization.en.lua
+++ DBM-Karazhan/localization.en.lua
@@ -316,7 +316,7 @@ L:SetMiscLocalization{
 	DBM_PRINCE_YELL_P2		= "Simple fools! Time is the fire in which you'll burn!",
 	DBM_PRINCE_YELL_P3		= "How can you hope to stand against such overwhelming power?",
 	DBM_PRINCE_YELL_INF1	= "All realities, all dimensions are open to me!",
-	DBM_PRINCE_YELL_INF2	= "You face not Malchezaar alone, but the legions I command!"
+	DBM_PRINCE_YELL_INF2	= "You face not Malchezaar alone, but the legions I command!",
 	DBM_PRINCE_YELL_INF3	= "The celestial forces are mine to manipulate."
 }
 
