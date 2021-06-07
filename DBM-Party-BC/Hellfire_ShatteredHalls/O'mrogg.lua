6fc477d185ab8696d7e7585fb341033a337ff258
Szyler <Szyler@Szyler.com>
2021-06-08 00:52:58 +0200

    Shattered Halls

--- DBM-Party-BC/Hellfire_ShatteredHalls/O'mrogg.lua
+++ DBM-Party-BC/Hellfire_ShatteredHalls/O'mrogg.lua
@@ -7,4 +7,29 @@ mod:SetCreatureID(16809)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_CAST_SUCCESS"
 )
+
+local timerFear		        = mod:NewNextTimer(30, 30584)
+local timerBlastWave		= mod:NewNextTimer(30, 30600)
+local timerThunderClap		= mod:NewNextTimer(30, 30633)
+
+function mod:OnCombatStart(delay)
+	timerBlastWave:Start(27)
+	timerFear:Start()
+	timerThunderClap:Start(15)
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 30584 then
+		timerFear:Start()
+	elseif args.spellId == 30600 then
+		timerBlastWave:Start()
+	elseif args.spellId == 30600 then
+		timerThunderClap:Start()
+	end
+end
+
+-- 30600 - Blast Wave
+-- 30633 - Thunderclap
+-- 30584 - Fear
