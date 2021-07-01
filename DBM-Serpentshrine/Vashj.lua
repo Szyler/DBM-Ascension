9361c1a422351b285e705d6b86ffacedd9f1bc17
Szyler <Szyler@Szyler.com>
2021-07-01 19:32:43 +0200

    Vashj from Junior

--- DBM-Serpentshrine/Vashj.lua
+++ DBM-Serpentshrine/Vashj.lua
@@ -15,29 +15,37 @@ mod:RegisterEvents(
 	"SPELL_CAST_SUCCESS",
 	"UNIT_DIED",
 	"CHAT_MSG_MONSTER_YELL",
-	"CHAT_MSG_LOOT"
+	"CHAT_MSG_LOOT",
+	"CHAT_MSG_RAID_BOSS_EMOTE"
 )
 
-local warnCharge		= mod:NewTargetAnnounce(38280, 4)
-local warnEntangle		= mod:NewSpellAnnounce(38316, 3)
+local warnCharge		= mod:NewTargetAnnounce(196779, 4)
+--local warnEntangle		= mod:NewSpellAnnounce(38316, 3)
 local warnPhase2		= mod:NewPhaseAnnounce(2)
 local warnElemental		= mod:NewAnnounce("WarnElemental", 4)
 local warnHydra			= mod:NewAnnounce("WarnHydra", 3)
 local warnNaga			= mod:NewAnnounce("WarnNaga", 3)
 --local warnShield		= mod:NewAnnounce("WarnShield", 3)
-local warnLoot			= mod:NewAnnounce("WarnLoot", 4)
-local warnLootYou		= mod:NewSpecialWarningYou(351397)
+--local warnLoot			= mod:NewAnnounce("WarnLoot", 4)
 local warnPhase3		= mod:NewPhaseAnnounce(3)
+local warnAimedShot		= mod:NewTargetAnnounce(196782, 4)
 
-local specWarnCharge	= mod:NewSpecialWarningMove(38280)
-local specWarnElemental	= mod:NewSpecialWarning("SpecWarnElemental")--Changed from soon to a now warning. the soon warning not accurate because of 11 second variation so not useful special warning.
+local specWarnCharge	= mod:NewSpecialWarningMove(196779)
+local specWarnDischarge	= mod:NewSpecialWarningMove(351379)
+-- local specWarnElemental	= mod:NewSpecialWarning("SpecWarnElemental")--Changed from soon to a now warning. the soon warning not accurate because of 11 second variation so not useful special warning.
 local specWarnToxic		= mod:NewSpecialWarningMove(38575)
+local specWarnHeal		= mod:NewSpecialWarning("SpecWarnHealer") -- 83565
 
-local timerCharge		= mod:NewTargetTimer(20, 38280)
-local timerElemental	= mod:NewTimer(22, "TimerElementalActive")--Blizz says they are active 20 seconds per patch notes, but my logs don't match those results. 22 second up time.
-local timerElementalCD	= mod:NewTimer(45, "TimerElemental")--46-57 variation. because of high variation the pre warning special warning not useful, fortunately we can detect spawns with precise timing.
-local timerHydra		= mod:NewTimer(63, "timerHydra")
-local timerNaga			= mod:NewTimer(47.5, "TimerNaga")
+local timerCharge		= mod:NewTimer(30, "Static Charge", 196779)
+local timerChargeDmg	= mod:NewTargetTimer(8, 196780)
+local timerAimedShot	= mod:NewTimer(30, "Aimed Shot", 196782)
+local timerMark			= mod:NewTargetTimer(8, 196782)
+-- local timerElemental	= mod:NewTimer(22, "TimerElementalActive")--Blizz says they are active 20 seconds per patch notes, but my logs don't match those results. 22 second up time.
+local timerElementalCD	= mod:NewTimer(75, "TimerElemental")--75-82 variation. because of high variation the pre warning special warning not useful, fortunately we can detect spawns with precise timing.
+local timerHydra		= mod:NewTimer(91, "TimerHydra")
+local timerNaga			= mod:NewTimer(47, "TimerNaga")
+local timerGenerator	= mod:NewTimer(30, "Next Generator", "Interface\\Icons\\Spell_Nature_LightningOverload")
+local timerDischarge	= mod:NewTimer(9, "Discharge", "Interface\\Icons\\Spell_Nature_LightningOverload")
 
 mod:AddBoolOption("RangeFrame", true)
 mod:AddBoolOption(L.ChargeIcon)
@@ -52,15 +60,15 @@ local elementals = {}
 function mod:HydraSpawn()
 	self.vb.hydraCount = self.vb.hydraCount + 1
 	timerHydra:Start(nil, tostring(self.vb.hydraCount))
-	warnHydra:Schedule(57, tostring(self.vb.hydraCount))
-	self:ScheduleMethod(63, "HydraSpawn")
+	warnHydra:Schedule(86, tostring(self.vb.hydraCount))
+	self:ScheduleMethod(91, "HydraSpawn")
 end
 
 function mod:NagaSpawn()
 	self.vb.nagaCount = self.vb.nagaCount + 1
 	timerNaga:Start(nil, tostring(self.vb.nagaCount))
-	warnNaga:Schedule(42.5, tostring(self.vb.nagaCount))
-	self:ScheduleMethod(47.5, "NagaSpawn")
+	warnNaga:Schedule(42, tostring(self.vb.nagaCount))
+	self:ScheduleMethod(47, "NagaSpawn")
 end
 
 function mod:OnCombatStart(delay)
@@ -79,8 +87,9 @@ function mod:OnCombatEnd()
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 38280 then
-		timerCharge:Start(args.destName)
+	if args.spellId == 196779 then
+		timerCharge:Start()
+		timerChargeDmg:Start(args.destName)
 		if args:IsPlayer() then
 			specWarnCharge:Show()
 			if self.Options.ChargeIcon then
@@ -95,14 +104,18 @@ function mod:SPELL_AURA_APPLIED(args)
 		if self.Options.ChargeIcon then
 			self:SetIcon(args.destName, 1, 20)
 		end
-	elseif args:IsSpellID(38575,85411) and args:IsPlayer() then
+	elseif args:IsSpellId(38575,85411) and args:IsPlayer() then
 		specWarnToxic:Show()
+	elseif args.spellId(196782) then
+		warnAimedShot:Show(args.destName)
+		timerMark:Start(args.destName)
+		timerAimedShot:Start()
 	end
 end
 
 function mod:SPELL_AURA_REMOVED(args)
-	if args.spellId == 38280 then
-		timerCharge:Stop(args.destName)
+	if args.spellId == 196779 then
+		timerChargeDmg:Stop(args.destName)
 		if self.Options.ChargeIcon then
 			self:SetIcon(args.destName, 0)
 		end
@@ -118,26 +131,40 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 
-function mod:SPELL_CAST_START(args)
-	if args.spellId == 38253 and not elementals[args.sourceGUID] then
-		specWarnElemental:Show()
-		timerElemental:Start()
-		elementals[args.sourceGUID] = true
-	end
-end
+--function mod:SPELL_CAST_START(args) -- useless now that we have raid boss emote telling us when Tainted spawns.
+--	if args:IsSpellId == 38253 and not elementals[args:sourceGUID] then
+--		specWarnElemental:Show()
+--		timerElemental:Start()
+--		elementals[args:sourceGUID] = true
+--	end
+--end
 
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 38316 then
-		warnEntangle:Show()
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(args)
+	if msg == DBM_VASHJ_DISCHARGE then
+		timerDischarge:Start()
+		specWarnDischarge:Show()
+	elseif msg == DBM_VASHJ_ELITE then
+		warnNaga:Show()
+		timerNaga:Start()
+	elseif msg == DBM_VASHJ_HYDRA then
+		warnHydra:Show()
+		timerHydra:Start()
+	elseif msg == DBM_VASHJ_TAINTED then
+		warnElemental:Show()
 	end
 end
+	
+--function mod:SPELL_CAST_SUCCESS(args)
+--	if args:IsSpellId == 38316 then
+--		warnEntangle:Show()
+--	end
+--end
 
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 22009 then
 		self.vb.elementalCount = self.vb.elementalCount + 1
-		timerElementalCD:Start(nil, tostring(self.vb.elementalCount))
-		warnElemental:Schedule(45, tostring(self.vb.elementalCount))
+		warnElemental:Schedule(70, tostring(self.vb.elementalCount))
 	end
 end
 
@@ -149,14 +176,16 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 		self.vb.elementalCount = 1
 		self.vb.shieldLeft = 4
 		warnPhase2:Show()
+		timerCharge:Cancel()
+		timerAimedShot:Cancel()
 		timerNaga:Start(nil, tostring(self.vb.nagaCount))
-		warnNaga:Schedule(42.5, tostring(self.vb.elementalCount))
-		self:ScheduleMethod(47.5, "NagaSpawn")
-		timerElementalCD:Start(nil, tostring(self.vb.elementalCount))
-		warnElemental:Schedule(45, tostring(self.vb.elementalCount))
-		timerHydra:Start(nil, tostring(self.vb.hydraCount))
-		warnHydra:Schedule(57, tostring(self.vb.hydraCount))
-		self:ScheduleMethod(63, "HydraSpawn")
+		warnNaga:Schedule(8, tostring(self.vb.elementalCount))
+		self:ScheduleMethod(13, "NagaSpawn")
+		timerElementalCD:Start(61, tostring(self.vb.elementalCount))
+		warnElemental:Schedule(56, tostring(self.vb.elementalCount))
+		timerHydra:Start(32, tostring(self.vb.hydraCount))
+		warnHydra:Schedule(28, tostring(self.vb.hydraCount))
+		self:ScheduleMethod(32, "HydraSpawn")
 	elseif msg == L.DBM_VASHJ_YELL_PHASE3 or msg:find(L.DBM_VASHJ_YELL_PHASE3) then
 		self.vb.phase = 3
 		warnPhase3:Show()
@@ -168,6 +197,8 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 		warnHydra:Cancel()
 		self:UnscheduleMethod("NagaSpawn")
 		self:UnscheduleMethod("HydraSpawn")
+		timerGenerator:Start()
+		timerCharge:Start(15)
 	end
 end
 
@@ -176,8 +207,8 @@ function mod:CHAT_MSG_LOOT(msg)
 	local player, itemID = msg:match(L.LootMsg)
 	if player and itemID and tonumber(itemID) == 31088 and self:IsInCombat() then
 		warnLoot:Show(player)
-		if player == UnitName("player") then
-			warnLootYou:Show()
-		end
+		--if player == UnitName("player") then
+		--		warnLootYou:Show()
+		--end
 	end
 end
