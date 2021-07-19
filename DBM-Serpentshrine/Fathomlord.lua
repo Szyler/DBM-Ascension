94b1dafb17d35aed2c0574131cd9e650d8bca631
Szyler <Szyler@Szyler.com>
2021-07-20 01:31:27 +0200

    Added Beast Within and Cataclysmic warning and timers

--- DBM-Serpentshrine/Fathomlord.lua
+++ DBM-Serpentshrine/Fathomlord.lua
@@ -8,18 +8,22 @@ mod:RegisterCombat("combat", 21214)
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
 	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS",
 	"SPELL_SUMMON"
 )
 
 local warnCariPower		= mod:NewSpellAnnounce(38451, 3)
 local warnTidalPower	= mod:NewSpellAnnounce(38452, 3)
 local warnSharPower		= mod:NewSpellAnnounce(38455, 3)
+local warnBeastWithin	= mod:NewTargetAnnounce(351373, 3)
 
 local specWarnHeal		= mod:NewSpellAnnounce(83535, 3)
 local specWarnTotem		= mod:NewSpecialWarning("Move from Totem!")
 
 local timerHeal			= mod:NewNextTimer(30, 83535)
 local timerFreeze		= mod:NewCDTimer(18, 38357)
+local timerCataclysmic	= mod:NewCDTimer(9, 38441)
+local timerBeastWithin	= mod:NewNextTimer(30, 351373)
 
 local berserkTimer		= mod:NewBerserkTimer(720)
 
@@ -41,6 +45,9 @@ function mod:SPELL_AURA_APPLIED(args)
 		else
 			timerFreeze:Start(30) -- Deep Freeze Fathom-Lord
 		end
+	elseif args:IsSpellID(351373) then -- Beast Within
+		warnBeastWithin:Show(args.destName)
+		timerBeastWithin:Start()
 	end
 end
 
@@ -48,6 +55,12 @@ function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(38330, 83535) then -- 83535
 		specWarnHeal:Show(args.sourceName)
 		timerHeal:Show()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(38441, 351365, 351366) then
+		timerCataclysmic:Start()
 	end
 end
 
