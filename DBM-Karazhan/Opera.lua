bc05df653ba0619f6df1bf5b4ce570ccc6d1b997
Szyler <Szyler@Szyler.com>
2021-08-27 12:58:26 +0200

    Juniors Opera fixes

--- DBM-Karazhan/Opera.lua
+++ DBM-Karazhan/Opera.lua
@@ -5,7 +5,7 @@ mod:SetRevision(("$Revision: 163 $"):sub(12, -3))
 
 
 mod:RegisterEvents(
-	"CHAT_MSG_RAID_WARNING"
+	"CHAT_MSG_RAID_BOSS_EMOTE"
 )
 
 local warningSpotlight		= mod:NewAnnounce(L.OperaSpotlight, 3, 85112)
@@ -14,7 +14,7 @@ local timerNextSpotlight	= mod:NewTimer(30, L.OperaSpotlight, 85112)
 local timerSpotlight		= mod:NewTimer(11, L.OperaSpotlightIn, 85112)
 local timerStageFright		= mod:NewTimer(15, L.OperaSpotNotUse, 85112)
 
-function mod:CHAT_MSG_RAID_WARNING(msg)
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 	if msg == L.STAGE_FRIGHT then
 		warningSpotlight:Show()
 		timerNextSpotlight:Start()
