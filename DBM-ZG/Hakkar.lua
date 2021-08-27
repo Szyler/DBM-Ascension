ef3735e83ee263bfbbaf185095221e5816ba2fdd
Szyler <Szyler@Szyler.com>
2021-08-27 12:01:34 +0200

    Timers and heroic/mythic spells

--- DBM-ZG/Hakkar.lua
+++ DBM-ZG/Hakkar.lua
@@ -7,47 +7,63 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
-	"SPELL_CAST_SUCCESS"
+	"SPELL_CAST_SUCCESS",
+	"SPELL_CAST_SUCCESS_DOSE"
+
 )
 
 local warnSiphonSoon	= mod:NewSoonAnnounce(24324)
+local timerSiphon		= mod:NewNextTimer(60, 24324)
+
 local warnInsanity		= mod:NewTargetAnnounce(24327)
-local warnBlood			= mod:NewTargetAnnounce(24328)
-
-local timerSiphon		= mod:NewNextTimer(90, 24324)
 local timerInsanity		= mod:NewTargetTimer(10, 24327)
-local timerInsanityCD	= mod:NewCDTimer(20, 24327)
-local timerBlood		= mod:NewTargetTimer(10, 24328)
+local timerNextInsanity	= mod:NewNextTimer(30, 24327)
 
+local warnBlood			= mod:NewTargetAnnounce(24328)
 local specWarnBlood		= mod:NewSpecialWarningYou(24328)
+local timerBlood		= mod:NewTargetTimer(16, 24328)
+
+local specWarnPool		= mod:NewSpecialWarningYou(340510)
 
 local enrageTimer		= mod:NewBerserkTimer(585)
 
 function mod:OnCombatStart(delay)
 	enrageTimer:Start(-delay)
-	warnSiphonSoon:Schedule(80-delay)
+	warnSiphonSoon:Schedule(55-delay)
 	timerSiphon:Start(-delay)
+	timerNextInsanity:Start(-delay)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(24327) then
 		warnInsanity:Show(args.destName)
 		timerInsanity:Start(args.destName)
-		timerInsanityCD:Start()
-	elseif args:IsSpellID(24328) then
+		timerNextInsanity:Start()
+	elseif args:IsSpellID(24328, 350416) or args:IsSpellID(350418, 350417) then
 		warnBlood:Show(args.destName)
 		timerBlood:Start(args.destName)
 		if args:IsPlayer() then
 			specWarnBlood:Show()
 		end
+	elseif args:IsSpellID(340510, 350419, 350420) then
+		if args:IsPlayer() then
+			specWarnPool:Show()
+		end
 	end
 end
+
+function mod:SPELL_AURA_APPLIED_DOSE(args)
+	if args:IsSpellID(340510, 350419, 350420) then
+		if args:IsPlayer() then
+			specWarnPool:Show()
+		end
+	end
+end	
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(24324) then
 		warnSiphonSoon:Cancel()
-		warnSiphonSoon:Schedule(80)
+		warnSiphonSoon:Schedule(55)
 		timerSiphon:Start()
 	end
 end
-		
