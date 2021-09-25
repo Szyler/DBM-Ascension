7e3e2a1ef4724fc95bc9afacb6ec17a0fb62d318
Szyler <Szyler@Szyler.com>
2021-09-25 20:16:02 +0100

    Geddon

--- DBM-MC/Geddon.lua
+++ DBM-MC/Geddon.lua
@@ -11,25 +11,30 @@ mod:RegisterEvents(
 	"SPELL_CAST_SUCCESS"
 )
 
-local warnInferno		= mod:NewSpellAnnounce(19695)
-local warnIgnite		= mod:NewSpellAnnounce(19659)
-local warnBomb			= mod:NewTargetAnnounce(20475)
-local warnArmageddon	= mod:NewSpellAnnounce(20478)
+local warnInferno			= mod:NewSpellAnnounce(19695)
+local warnBomb				= mod:NewTargetAnnounce(20475)
+local specWarnBomb			= mod:NewSpecialWarningYou(20475)
+-- local warnIgnite			= mod:NewSpellAnnounce(19659)
+-- local warnArmageddon		= mod:NewSpellAnnounce(20478)
 
-local timerInferno		= mod:NewCastTimer(8, 19695)
-local timerBomb			= mod:NewTargetTimer(8, 20475)
-local timerIgnite		= mod:NewBuffActiveTimer(300, 19659)
-local timerArmageddon	= mod:NewCastTimer(8, 20478)
+local timerNextInferno		= mod:NewNextTimer(55, 19695)
+local timerNextLiving		= mod:NewNextTimer(16, 20475)
 
-local specWarnBomb		= mod:NewSpecialWarningYou(20475)
+local timerInferno			= mod:NewCastTimer(8, 19695)
+local timerBomb				= mod:NewTargetTimer(8, 20475)
+-- local timerIgnite		= mod:NewBuffActiveTimer(300, 19659)
+-- local timerArmageddon	= mod:NewCastTimer(8, 20478)
+
 
 mod:AddBoolOption("SetIconOnBombTarget", true)
 
 function mod:OnCombatStart(delay)
+	timerNextInferno:Start(30-delay)
+	timerNextLiving:Start(15-delay)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(20475) then
+	if args:IsSpellID(20475, 2105061) then
 		timerBomb:Start(args.destName)
 		warnBomb:Show(args.destName)
 		if self.Options.SetIconOnBombTarget then
@@ -42,15 +47,16 @@ function mod:SPELL_AURA_APPLIED(args)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(19695) then
+	if args:IsSpellID(19695, 2105059) then
 		warnInferno:Show()
 		timerInferno:Start()
-	elseif args:IsSpellID(19659) then
-		warnIgnite:Show()
-		timerIgnite:Start()
-	elseif args:IsSpellID(20478) then
-		warnArmageddon:Show()
-		timerArmageddon:Start()
+		timerNextInferno:Start()
+	-- elseif args:IsSpellID(19659) then
+	-- 	warnIgnite:Show()
+	-- 	timerIgnite:Start()
+	-- elseif args:IsSpellID(20478) then
+	-- 	warnArmageddon:Show()
+	-- 	timerArmageddon:Start()
 	end
 end
 
