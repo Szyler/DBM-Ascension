6cdf165077f4850aed9b57b4118bb7b604a9ff61
Barsk <barsk@barsk.com>
2021-10-06 13:14:32 +0200

    Update Onyxia.lua
    Updated Health thresholds for Phase warning.
    Added Blast Nova timer (+/- a few seconds, as it is not static but variable depending on Guard movement before casting, so use it as an indicator)
    Added Special warning to Blast Nova.
    Added Special warning for Deep Breath.
    Upgraded the logic around next Blast Nova timer and the internal CD on Blast Nova.
    Added warning for Bellowing Roar.
    Added timer for next bellowing Roar.

--- DBM-Onyxia/Onyxia.lua
+++ DBM-Onyxia/Onyxia.lua
@@ -17,17 +17,18 @@ local warnPhase2			= mod:NewPhaseAnnounce(2)
 local warnPhase3			= mod:NewPhaseAnnounce(3)
 local warnPhase2Soon		= mod:NewAnnounce("Phase 2 Soon", 1)
 local warnPhase3Soon		= mod:NewAnnounce("Phase 3 Soon", 1)
-local warnBlastNova			= mod:NewCastAnnounce(2105147)
 
 local specWarnBlastNova		= mod:NewSpecialWarningMove(2105147)
+local specWarnDeepBreath	= mod:NewSpecialWarningMove(18609)
 
 local timerNextDeepBreath	= mod:NewCDTimer(35, 17086)--Range from 35-60seconds in between based on where she moves to.
 local timerBreath			= mod:NewCastTimer(8, 17086)
 local timerBlastNova		= mod:NewCastTimer(4, 2105147)
-local timerNextBlastNova	= mod:NewCDTimer(32, 2105147)
-local timerIntBlastNova1	= mod:NewCDTimer(40, 2105147)
-local timerIntBlastNova2	= mod:NewCDTimer(40, 2105147)
-
+local timerBellowingRoar	= mod:NewCastTimer(4, 18431)
+local timerNextBellowingRoar= mod:NewCDTimer(45, 18431)
+local timerNextBlastNova	= mod:NewCDTimer(32, 2105147)-- +/- a few seconds as it varies depending on Guard movement.
+local timerIntBlastNova1	= mod:NewCDTimer(40, 2105147)-- Internal CD for a Guard to cast Blast Nova twice
+local timerIntBlastNova2	= mod:NewCDTimer(40, 2105147)-- second one for secondary add
 local lastBlastNova = 0
 
 local prewarnP2
@@ -61,31 +62,37 @@ end
 function mod:CHAT_MSG_MONSTER_YELL(msg)
 	if msg == L.YellP2 or msg:find(L.YellP2) then
 		timerNextDeepBreath:Start(77)
-		timerNextBlastNova:Start(17)
+		timerNextBlastNova:Start()
 	elseif msg == L.YellP3 or msg:find(L.YellP3) then
 		timerNextDeepBreath:Stop()
+		timerNextBellowingRoar:Start(8)
 	end
 end
 
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(17086, 18351, 18564, 18576) or args:IsSpellID(18584, 18596, 18609, 18617) then	-- 1 ID for each direction
+		specWarnDeepBreath:Show()
 		timerBreath:Start()
 		timerNextDeepBreath:Start()
 	end
 	if args:IsSpellID(2105145, 2105146, 2105147, 2105148) then
-		warnBlastNova:Show()
 		specWarnBlastNova:Show()
 		timerBlastNova:Start()
 		lastBlastNova = GetTime()
-		if (GetTime() - lastBlastNova) <= 1 or (GetTime() - lastBlastNova) > 15  then
+		if (GetTime() - lastBlastNova) <= 1 or (GetTime() - lastBlastNova) > 15  then --Trying it to only trigger from new Add Blast Nova.
 			timerNextBlastNova:Start()
 		end
-		if timerIntBlastNova1:GetTime() > 0 then
+		if (timerNextBlastNova:GetTime() >= 37 and timerIntBlastNova1:GetTime() > 30) or (timerNextBlastNova:GetTime() <= 3 and timerIntBlastNova1:GetTime() > 30) then
 			timerIntBlastNova2:Start()
-		else
+		elseif
+			timerNextBlastNova:GetTime() >= 37 and (timerIntBlastNova2:GetTime() == nil or timerIntBlastNova2:GetTime() > 30) or timerNextBlastNova:GetTime() <= 3 and (timerIntBlastNova2:GetTime() == nil or timerIntBlastNova2:GetTime() > 30) then --Trying to only get the Internal CD on the Nova closest to the timer.
 			timerIntBlastNova1:Start()
 		end
-		
+	end
+	if args:IsSpellID(18431) then
+		timerBellowingRoar:Start()
+		timerNextBellowingRoar:Stop()
+		timerNextBellowingRoar:Start()
 	end
 end
 
@@ -102,17 +109,5 @@ function mod:UNIT_HEALTH(uId)
 	elseif self:GetUnitCreatureId(uId) == 10184 and UnitHealth(uId) / UnitHealthMax(uId) <= 0.35 and warnP3 == 0 then			
 		warnP3 = 1
 		self:ScheduleMethod(0, "alertP3")
-	end
-	if mod:IsDifficulty("heroic10", "heroic25") then
-		if self:GetUnitCreatureId(uId) == 36561 and UnitHealth(uId) / UnitHealthMax(uId) <= 0.02 then
-			if  timerIntBlastNova2:GetTime() > 32 or timerIntBlastNova2:GetTime() < 28 and timerIntBlastNova2:GetTime() < timerIntBlastNova1:GetTime() then
-				timerIntBlastNova2:Stop()
-			else
-				timerIntBlastNova1:Stop()
-			end
-		end
-	else if self:GetUnitCreatureId(uId) == 36561 and UnitHealth(uId) / UnitHealthMax(uId) <= 0.1 then
-			timerIntBlastNova1:Stop()
-		end
 	end
 end
