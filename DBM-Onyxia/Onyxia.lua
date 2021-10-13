0fd54a323761538f08fa932d9ab4cdce88d8f6f0
Barsk <barsk@barsk.com>
2021-10-13 21:56:22 +0200

    Lair Guard and Whelp timers
    reworked the Lair Guard and Blast Nova timers

--- DBM-Onyxia/Onyxia.lua
+++ DBM-Onyxia/Onyxia.lua
@@ -18,21 +18,25 @@ local warnPhase3			= mod:NewPhaseAnnounce(3)
 local warnPhase2Soon		= mod:NewAnnounce("Phase 2 Soon", 1)
 local warnPhase3Soon		= mod:NewAnnounce("Phase 3 Soon", 1)
 local warnFireball			= mod:NewTargetAnnounce(2105160, 3)
+local warnBlastNova	 		= mod:NewCastAnnounce(2105147)
+local WarnGuardSpawn		= mod:NewAnnounce("Lair Guard spawned")
+local WarnWhelps			= mod:NewAnnounce("Onyxian Whelps spawned")
 
-local specWarnBlastNova		= mod:NewSpecialWarningMove(2105147)
-local specWarnDeepBreath	= mod:NewSpecialWarningMove(18609)
-local specWarnFireballYou	= mod:NewSpecialWarningYou(2105160)
+local specWarnDeepBreath	= mod:NewSpecialWarningMove(18609, 2)
+local specWarnFireballYou	= mod:NewSpecialWarningYou(2105160, 2)
 
 local timerBreath			= mod:NewCastTimer(8, 17086)
 local timerBlastNova		= mod:NewCastTimer(4, 2105147)
 local timerBellowingRoar	= mod:NewCastTimer(4, 18431)
 local timerFireball			= mod:NewCastTimer(4, 2105160)
 
-local timerNextDeepBreath	= mod:NewCDTimer(75, 17086)--Range from 35-60seconds in between based on where she moves to.
+local timerGuardSpawn		= mod:NewCDTimer(35, "Next Lair Guard")
+local timerWhelps			= mod:NewCDTimer(100, "Next Onyxian Whelps")
+local timerNextDeepBreath	= mod:NewCDTimer(75, 17086)--Range from 75-80 seconds in between based on where she moves to.
 local timerNextBellowingRoar= mod:NewCDTimer(45, 18431)
-local timerNextBlastNova	= mod:NewCDTimer(32, 2105147)-- +/- a few seconds as it varies depending on Guard movement.
-local timerIntBlastNova1	= mod:NewCDTimer(40, 2105147)-- Internal CD for a Guard to cast Blast Nova twice
-local timerIntBlastNova2	= mod:NewCDTimer(40, 2105147)-- second one for secondary add
+local timerNextBlastNova	= mod:NewCDTimer(15, 2105147)-- +/- a few seconds as it varies depending on Guard movement.
+local timerIntBlastNova		= mod:NewCDTimer(40, 2105147)-- Internal CD for a Guard to cast Blast Nova twice
+-- local timerIntBlastNova2	= mod:NewCDTimer(40, 2105147)-- second one for secondary add
 
 local lastBlastNova = 0
 
@@ -67,12 +71,11 @@ end
 function mod:CHAT_MSG_MONSTER_YELL(msg)
 	if msg == L.YellP2 or msg:find(L.YellP2) then
 		timerNextDeepBreath:Start(77)
-		timerNextBlastNova:Start()
+		self:ScheduleMethod(15,"LairGuardSpawn")
 	elseif msg == L.YellP3 or msg:find(L.YellP3) then
 		timerNextDeepBreath:Stop()
-		timerBlastNova:Stop()
-		timerIntBlastNova1:Stop()
-		timerIntBlastNova2:Stop()
+		timerNextBlastNova:Stop()
+		timerIntBlastNova:Stop()
 		timerNextBellowingRoar:Start(8)
 	end
 end
@@ -87,7 +90,24 @@ function mod:MassiveFireball()
 		warnFireball:Show(target)
 	end
 	timerFireball:Start(target)
-	self:SetIcon(target, 3, 4)
+	if self.Options.FireballMark then
+		self:SetIcon(target, 8, 3)
+	end
+end
+
+function mod:LairGuardSpawn()							
+	self:UnscheduleMethod("LairGuardSpawn")
+	WarnGuardSpawn:Show()
+	timerGuardSpawn:Start()
+	timerNextBlastNova:Start()
+	self:ScheduleMethod(35,"LairGuardSpawn")
+end
+
+function mod:WhelpsSpawn()							
+	self:UnscheduleMethod("WhelpsSpawn")
+	WarnWhelps:Show()
+	timerWhelps:Start()
+	self:ScheduleMethod(100,"WhelpsSpawn")
 end
 
 function mod:SPELL_CAST_START(args)
@@ -98,18 +118,13 @@ function mod:SPELL_CAST_START(args)
 		timerNextDeepBreath:Start()
 	end
 	if args:IsSpellID(2105158, 2105159, 2105160, 2105161) then
-		self:ScheduleMethod(0.25, "MassiveFireball")
+		self:ScheduleMethod(0.4, "MassiveFireball")
 	end
 	if args:IsSpellID(2105145, 2105146, 2105147, 2105148) then
-		specWarnBlastNova:Show()
+		warnBlastNova:Show()
 		timerBlastNova:Start()
-		if (GetTime() - lastBlastNova) > 20  then --Trying it to only trigger from new Add Blast Nova.
-			timerNextBlastNova:Start()
-		end
-		if timerNextBlastNova:GetTime() >= 37 and timerNextBlastNova:GetTime() <= 3 then
-			timerIntBlastNova1:Start()
-		else
-			timerIntBlastNova2:Start()
+		if timerGuardSpawn > 15  then --Trying it to only trigger from new Add Blast Nova.
+			timerIntBlastNova:Start()
 		end
 		lastBlastNova = GetTime()
 	end
--- DBM-Onyxia/localization.en.lua
+++ DBM-Onyxia/localization.en.lua
@@ -10,21 +10,27 @@ L:SetGeneralLocalization{
 }
 
 L:SetWarningLocalization{
-	WarnWhelpsSoon		= "Onyxian Whelps soon",
+--  WarnWhelpsSoon		= "Onyxian Whelps soon",
 	WarnPhase2Soon		= "Phase 2 soon",
-	WarnPhase3Soon		= "Phase 3 soon"
+	WarnPhase3Soon		= "Phase 3 soon",
+	WarnGuardSpawn		= "Onyxian Lair Guard spawned",
+	WarnWhelps			= "Onyxian Whelps spawned"
 }
 
 L:SetTimerLocalization{
-	TimerWhelps	= "Onyxian Whelps"
+	TimerWhelps			= "Onyxian Whelps",
+	TimerGuardSpawn 	= "Lair Guard",
 }
 
 L:SetOptionLocalization{
 	TimerWhelps				= "Show timer for Onyxian Whelps",
-	WarnWhelpsSoon			= "Show pre-warning for Onyxian Whelps",
+	TimerGuardSpawn			= "Show timer for Onyxian Lair Guards spawning",
+--  WarnWhelpsSoon			= "Show pre-warning for Onyxian Whelps",
+	WarnWhelps				="Show warning for Onyxian Whelps spawning",
 	SoundWTF				= "Play some funny sounds from a legendary classic Onyxia raid",
 	WarnPhase2Soon			= "Show pre-warning for Phase 2 (at ~67%)",
-	WarnPhase3Soon			= "Show pre-warning for Phase 3 (at ~41%)"
+	WarnPhase3Soon			= "Show pre-warning for Phase 3 (at ~41%)",
+	FireballMark		 		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(2105160),
 }
 
 L:SetMiscLocalization{
