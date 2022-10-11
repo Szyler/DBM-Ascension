1bdee5da69c9bd62187ed74899b6eec35d10cd69
Szyler <Szyler@Szyler.com>
2022-10-11 02:22:28 +0200

    Fixing fire bombs timer warning and cast timer

--- DBM-ZulAman/Jan'alai.lua
+++ DBM-ZulAman/Jan'alai.lua
@@ -19,14 +19,14 @@ local warnAddsSoon			= mod:NewSoonAnnounce(43962, 3)
 
 local warnFlame				= mod:NewSpecialWarningRun(2135924)
 local specWarnAdds			= mod:NewSpecialWarningSpell(43962)
-local specWarnBomb			= mod:NewSpecialWarningSpell(2135933)
-
-local timerNextBomb			= mod:NewNextTimer(37, 2135933)--Cast bar?
-local bombCast				= mod:NewCastTimer(7, 2135933)
 local timerNextAdds			= mod:NewNextTimer(57, 43962)
+
+local specWarnBomb			= mod:NewSpecialWarningSpell(2135915)
+local timerNextBomb			= mod:NewNextTimer(37, 2135915)--Cast bar? 2135913 summon, 2135914 filler, Damage: 2135915, 2135933, 2135934, 2135935
+local timerBombCast			= mod:NewCastTimer(6, 2135913) --2135913 summon, 2135914 filler, Damage: 2135915, 2135933, 2135934, 2135935
 local timerNextBreath		= mod:NewNextTimer(15, 2135901)
 local timerNextFlameWhirl	= mod:NewNextTimer(32, 2135908)
-local flameWhirlCast		= mod:NewCastTimer(6, 2135908)
+local timerFlameWhirlCast	= mod:NewCastTimer(6, 2135908)
 local timerNextPillar		= mod:NewNextTimer(20, 2135923) --2135923, 2135924, 2135925, 2135926, 2135927
 
 local WarnAddsCount			= mod:NewAnnounce(L.Hatchlings, 2, 85178) --2135905, 2135906, 2135918
@@ -37,18 +37,21 @@ local berserkTimer			= mod:NewBerserkTimer(600)
 local pillarTimer = 0
 local hatchlingsAlive = 0
 local hathclingsSpawned = 0
+local bombNextTimer = 0
 
 mod:SetUsedIcons(1)
 mod:AddBoolOption("FlameIcon")
 
 function mod:OnCombatStart(delay)
 	timerNextAdds:Start(20)
-	timerNextBomb:Start(30)
+	timerNextBomb:Start(33)
 	timerNextBreath:Start(5)
 	berserkTimer:Start(-delay)
 	pillarTimer = 15
 	hatchlingsAlive = 0
 	hathclingsSpawned = 0
+	bombNextTimer = 25
+	self.vb.phase = 1
 end
 
 function mod:SPELL_CAST_START(args)
@@ -70,6 +73,18 @@ function mod:SPELL_CAST_SUCCESS(args)
 	elseif args:IsSpellID(2135905) then
 		specWarnAllAdds:Show()
 		timerAllAdds:Start()
+	elseif args:IsSpellID(2135913) then
+		timerBombCast:Start()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(2135907) then
+		self.vb.phase = 2
+		specWarnBomb:Schedule(bombNextTimer-6)
+		timerBombCast:Schedule(bombNextTimer-6)
+		timerNextBomb:Start(bombNextTimer-6)
+		bombNextTimer = 22
 	end
 end
 
@@ -90,6 +105,11 @@ function mod:SPELL_DAMAGE(args)
 			pillarTimer = ((pillarTimer + 5) <= 20) and 20 or 15 -- Alternates between 20 and 15. If pillartimer + 5 is 20, then 20 otherwise (ie. if 20+5 is not 20, then) 15
 			timerNextPillar:Start(pillarTimer) --
 		end
+	elseif args:IsSpellID(2135915, 2135933, 2135934, 2135935) and AntiSpam(1, 2) and self.vb.phase == 2 then
+		specWarnBomb:Schedule(bombNextTimer-6)
+		timerBombCast:Schedule(bombNextTimer-6)
+		timerNextBomb:Start(bombNextTimer-6)
+		bombNextTimer = ((bombNextTimer + 6) <= 22) and 22 or 16
 	end
 end
 
@@ -121,9 +141,6 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 		timerNextAdds:Start()
 	elseif msg == L.YellBomb or msg:find(L.YellBomb) then
 		--local elapsed, total = timerNextBreath:GetTime();
-		timerNextBreath:AddTime(7)
-		specWarnBomb:Show()
-		bombCast:Start()
-		timerNextBomb:Start()
+		timerNextBreath:AddTime(6)
 	end
 end
