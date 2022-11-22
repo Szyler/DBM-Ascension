eb282f26bbf44d61bf29ce936dd16360027edb5e
Szyler <Szyler@Szyler.com>
2022-11-22 21:59:33 +0100

    Lurker new ids

--- DBM-Serpentshrine/TheLurkerBelow.lua
+++ DBM-Serpentshrine/TheLurkerBelow.lua
@@ -15,22 +15,22 @@ mod:RegisterEvents(
 
 local warnSubmerge		= mod:NewAnnounce("WarnSubmerge", 2, "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp")
 local warnEmerge		= mod:NewAnnounce("WarnEmerge", 1, "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp")
-local warnWhirl			= mod:NewSpellAnnounce(37363, 2)
-local warnFocusedFire	= mod:NewTargetAnnounce(351300, 2)
+local warnWhirl			= mod:NewSpellAnnounce(2137605, 2)
+local warnFocusedFire	= mod:NewTargetAnnounce(2137622, 2)
 
-local warnFocusedYou	= mod:NewSpecialWarningYou(351300)
-local specWarnSpout		= mod:NewSpecialWarningSpell(37433)
+local warnFocusedYou	= mod:NewSpecialWarningYou(2137622)
+local specWarnSpout		= mod:NewSpecialWarningSpell(2137607)
 
 local timerSubmerge		= mod:NewTimer(135, "TimerSubmerge", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp")
 local timerEmerge		= mod:NewTimer(60, "TimerEmerge", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp")
-local timerNextSpout	= mod:NewNextTimer(70, 37433) --heroic: 351337, Mythic:351338
-local timerSpout		= mod:NewBuffActiveTimer(22, 37433)
-local timerNextWhirl	= mod:NewNextTimer(20, 37363) --Whirl has 20s next timer, but pushed back by other casts. Need "GetTimer" to push it back (see maiden/mag)
-local timerGeyser		= mod:NewNextTimer(20, 37478) --heroic: 351335, Mythic:351336
+local timerNextSpout	= mod:NewNextTimer(70, 2137607) 
+local timerSpout		= mod:NewBuffActiveTimer(22, 2137607)
+local timerNextWhirl	= mod:NewNextTimer(20, 2137605) --Whirl has 20s next timer, but pushed back by other casts. Need "GetTimer" to push it back (see maiden/mag)
+local timerGeyser		= mod:NewNextTimer(20, 2137611) 
 
 --Ascended mechanics:
 local timerTentacle		= mod:NewNextTimer(10, 26391) 
-local warnSmash			= mod:NewSpecialWarningSpell(351322)
+local warnSmash			= mod:NewSpecialWarningSpell(2137626)
 
 function mod:OnCombatStart(delay)
 	submerged = false
@@ -44,7 +44,7 @@ function mod:SPELL_AURA_APPLIED(args)
 	-- if args:IsSpellID(7731) then
 	-- 	DBM:AddMsg("Fishing applied - Pull inc")
 	-- else
-	if args:IsSpellID(351300) then
+	if args:IsSpellID(2137622) then
 		if args.destName == UnitName("player") then
 			warnFocusedYou:Show()
 		else
@@ -81,23 +81,23 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, src)
 end
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(351322) then
+	if args:IsSpellID(2137626) then
 		warnSmash:Show()
 	end
 end
 
 function mod:SPELL_DAMAGE(args)
-	if args:IsSpellID(37478, 351335, 351336) then
+	if args:IsSpellID(2137611, 2137612, 2137613, 2137614) then
 		timerGeyser:Start()
-	elseif args:IsSpellID(37363) then
+	elseif args:IsSpellID(2137605) then
 		timerNextWhirl:Start()
 	end
 end
 
 function mod:SPELL_MISSED(args)
-	if args:IsSpellID(37478, 351335, 351336) then
+	if args:IsSpellID(2137611, 2137612, 2137613, 2137614) then
 		timerGeyser:Start()
-	elseif args:IsSpellID(37363) then
+	elseif args:IsSpellID(2137605) then
 		timerNextWhirl:Start()
 	end
 end
