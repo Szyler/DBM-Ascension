46621cc073ccceb592df14b7d8a2df8cf9696e92
Szyler <Szyler@Szyler.com>
2022-11-22 21:22:27 +0100

    Lurker new spellIDs

--- DBM-Serpentshrine/Hydross.lua
+++ DBM-Serpentshrine/Hydross.lua
@@ -15,20 +15,20 @@ mod:RegisterEvents(
 local warnMarkF			= mod:NewAnnounce(L.WarnMark, 3, 351203)
 local warnMarkN			= mod:NewAnnounce(L.WarnMark, 3, 351204)
 local warnPhase			= mod:NewAnnounce("WarnPhase", 4)
-local warnTomb			= mod:NewTargetAnnounce(38235, 3)
+local warnTomb			= mod:NewTargetAnnounce(2137505, 3)
 local specWarnTidal		= mod:NewSpecialWarning("Tidalwave, stack!")
-local warnSludge		= mod:NewTargetAnnounce(38246, 2)--Maybe filter it some if spammy?
+local warnSludge		= mod:NewTargetAnnounce(2137509, 2)--Maybe filter it some if spammy?
 local warnTidalPower	= mod:NewAnnounce(L.WarnMark, 3, 351204)
 
 -- local specWarnMark	= mod:NewSpecialWarning("SpecWarnMark")
 
-local timerNextTomb		= mod:NewNextTimer(45, 38235)
-local timerNextSludge	= mod:NewNextTimer(45, 38246)
-local timerNextTidal	= mod:NewNextTimer(45, 85416)
-local timerTidal1		= mod:NewTimer(6, "Tidal Wave (1)", 85416)
-local timerTidal2		= mod:NewTimer(6, "Tidal Wave (2)", 85416)
-local timerTidal3		= mod:NewTimer(6, "Tidal Wave (3)", 85416)
-local timerSludge		= mod:NewTargetTimer(12, 38246)
+local timerNextTomb		= mod:NewNextTimer(45, 2137505)
+local timerNextSludge	= mod:NewNextTimer(45, 2137509)
+local timerNextTidal	= mod:NewNextTimer(45, 2137514)
+local timerTidal1		= mod:NewTimer(6, "Tidal Wave (1)", 2137514)
+local timerTidal2		= mod:NewTimer(6, "Tidal Wave (2)", 2137514)
+local timerTidal3		= mod:NewTimer(6, "Tidal Wave (3)", 2137514)
+local timerSludge		= mod:NewTargetTimer(12, 2137509)
 -- local timerMark		= mod:NewTimer(15, "TimerMark", 351203)
 
 local berserkTimer		= mod:NewTimer(600, "Berserk", 26662)
@@ -58,7 +58,7 @@ end
 function mod:PhaseChangeAddTime()
 	local elapsed, total = timerNextTidal:GetTime();
 	local currentRemainingTidalTimer = total - elapsed
-	timerNextTidal:AddTime(2)
+	timerNextTidal:AddTime(2.5)
 	self:UnscheduleMethod("tidalWave")
 	self:ScheduleMethod(currentRemainingTidalTimer+2, "tidalWave")
 	berserkTimer:AddTime(2)
@@ -85,7 +85,7 @@ function mod:SPELL_AURA_APPLIED(args)
 	-- elseif args.spellId == 351203 then
 	-- 	timerMark:Cancel()
 	-- 	timerMark:Start()
-	if args:IsSpellID(37961) then -- Corruption transform on boss
+	if args:IsSpellID(2137504) then -- Corruption transform on boss
 		warnPhase:Show(L.Nature)
 		timerNextTomb:Stop()
 		if GetTime() - LastTombSludge <= 32 then
@@ -99,7 +99,7 @@ function mod:SPELL_AURA_APPLIED(args)
 end
 
 function mod:SPELL_AURA_REMOVED(args)
-	if args:IsSpellID(37961) then -- Losing Corruption transform on boss
+	if args:IsSpellID(2137504) then -- Losing Corruption transform on boss
 		warnPhase:Show(L.Frost)
 		timerNextSludge:Stop()
 		if GetTime() - LastTombSludge <= 32 then
@@ -113,17 +113,17 @@ function mod:SPELL_AURA_REMOVED(args)
 end
 
 function mod:SPELL_AURA_APPLIED_DOSE(args)
-	if 	args:IsSpellID(351203, 351286, 351287) then	-- Heroic: 351286, Mythic: 351287 --Hydros
+	if 	args:IsSpellID(2137523, 2137524, 2137525, 2137526) then	 -- Mark of Hydross
 		if args.amount and (GetTime() - lastMarkF) > 2 and args.amount >= 10 and args.amount % 5 == 0 then
 			lastMarkF = GetTime()
 			warnMarkF:Show(args.amount, args.spellName)
 		end
-	elseif args:IsSpellID(351204, 351288, 351289) then   	-- Heroic: 351288, Mythic: 351289 --Corruption
+	elseif args:IsSpellID(2137527, 2137528, 2137529, 2137530) then   	  -- Mark of Corruption
 		if args.amount and (GetTime() - lastMarkN) > 2 and args.amount >= 10 and args.amount % 5 == 0 then
 			lastMarkN = GetTime()
 			warnMarkN:Show(args.amount, args.spellName)
 		end
-	elseif args:IsSpellID(85413) then   	-- Heroic: 351288, Mythic: 351289 --Corruption
+	elseif args:IsSpellID(2137514) then   	-- Tidal Power
 		if args.amount and (GetTime() - lastTidalPower) > 2 and args.amount >= 75 and args.amount % 5 == 0 then
 			lastTidalPower = GetTime()
 			warnTidalPower:Show(args.amount, args.spellName)
@@ -132,17 +132,14 @@ function mod:SPELL_AURA_APPLIED_DOSE(args)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(38235, 351290, 351291) then
+	if args:IsSpellID(2137505, 2137506, 2137507, 2137508) then
 		warnTomb:Show(args.destName)
 		timerNextTomb:Start()
 		LastTombSludge = GetTime()
-	elseif args:IsSpellID(38246, 351292, 351293) then
+	elseif args:IsSpellID(2137509, 2137510, 2137511, 2137512) then
 		warnSludge:Show(args.destName)
 		timerSludge:Start(args.destName)
 		timerNextSludge:Start()
 		LastTombSludge = GetTime()
 	end
 end
-
--- 351203 - Mark of Hydross
--- 351204 - Mark of Corruption
