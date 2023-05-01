bf78bd39b0f6b5d44108e202e6ce8e7c20ca6a11
strineX <87068357+strineX@users.noreply.github.com>
2023-05-01 16:24:39 +0200

    Revert "Updated Spell IDs and added some functions"
    This reverts commit 46d78b47dcde54671e4171d7d3cedea029c99c17.

--- DBM-Naxx/ArachnidQuarter/Maexxna.lua
+++ DBM-Naxx/ArachnidQuarter/Maexxna.lua
@@ -21,10 +21,9 @@ local warnNecrotic				= mod:NewAnnounce(L.MaexxnaNecrotic, 2, 28798)
 local warnWebWrap			= mod:NewTargetAnnounce(28622, 2)
 local timerWebWrap			= mod:NewNextTimer(40, 28622)
 -----WEB SPRAY-----
-local warnWebSpraySoon		= mod:NewSoonAnnounce(2123206, 1)
-local warnWebSprayNow		= mod:NewSpellAnnounce(2123206, 3)
-local timerWebSpray			= mod:NewNextTimer(40, 2123206)
-local timerWebStun			=mod:NewCastTimer(4,2123210)
+local warnWebSpraySoon		= mod:NewSoonAnnounce(29484, 1)
+local warnWebSprayNow		= mod:NewSpellAnnounce(29484, 3)
+local timerWebSpray			= mod:NewNextTimer(40, 29484)
 -----SPIDERLINGS-----
 local timerSpider			= mod:NewNextTimer(16, 43134)
 -----SOFT ENRAGE-----
@@ -47,20 +46,19 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 		timerSpider:Start(timer)
 	end
 end
-function SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(2123206) then -- Web Spray
-	warnWebSprayNow:Show()
-	timerWebStun:start()
-	timerWebSpray:Start(50)
-	end
-end
+
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(2123211,2123212,2123216,2123217) then -- Web Wrap
+	if args:IsSpellID(28622) then -- Web Wrap
 		warnWebWrap:Show(args.destName)
 		timerWebWrap:Start()
 		if args.destName == UnitName("player") then
 			SendChatMessage(L.YellWebWrap, "YELL")
 		end
+	elseif args:IsSpellID(29484, 54125, 350287) then -- Web Spray
+		timer = 40
+		warnWebSprayNow:Show()
+		warnWebSpraySoon:Schedule(timer-5)
+		timerWebSpray:Start(timer)
 	elseif args:IsSpellID(28776) then
 		warnNecrotic:Show(args.spellName, args.destName, args.amount or 1)
 		timerNecrotic:Start()
@@ -68,7 +66,7 @@ function mod:SPELL_AURA_APPLIED(args)
 end
 
 function mod:SPELL_AURA_APPLIED_DOSE(args)
-	if args:IsSpellID(2123202) then
+	if args:IsSpellID(28776) then
 		warnNecrotic:Show(args.spellName, args.destName, args.amount or 1)
 	end
 end
--- DBM-Naxx/MilitaryQuarter/Horsemen.lua
+++ DBM-Naxx/MilitaryQuarter/Horsemen.lua
@@ -2,12 +2,11 @@ local mod	= DBM:NewMod("Horsemen", "DBM-Naxx", 4)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 2248 $"):sub(12, -3))
-mod:SetCreatureID(16063, 16064, 16065, 16062)
-mod:RegisterCombat("combat", 16063, 16064, 16065, 16062)
+mod:SetCreatureID(16063, 16064, 16065, 30549)
+mod:RegisterCombat("combat", 16063, 16064, 16065, 30549)
 mod:EnableModel()
 mod:RegisterEvents(
 	"SPELL_CAST_SUCCESS",
-	"SPELL_CAST_START",
 	"SPELL_AURA_APPLIED_DOSE",
 	"PLAYER_ALIVE"
 )
@@ -16,20 +15,12 @@ mod:RegisterEvents(
 local warnMarkSoon			= mod:NewAnnounce("WarningMarkSoon", 1, 28835, false)
 local warnMarkNow			= mod:NewAnnounce("WarningMarkNow", 2, 28835)
 local specWarnMarkOnPlayer	= mod:NewSpecialWarning("SpecialWarningMarkOnPlayer", nil, false, true)
-local specWarnHolyWrathYou	= mod:NewSpecialWarningYou(2124141,2)
-local warnHolyWrath			= mod:NewTargetAnnounce(2124141,3)
-local specWarnDeepChillYou	= mod:NewSpecialWarningYou(2124167,2)
-local warnDeepChill			= mod:NewTargetAnnounce(2124167,3)
-
-----timers----
-local timerHolyWrath		= mod:NewCastTimer(4, 2124141)
-local timerDeepChill		= mod:NewCastTimer(4, 2124167)
 -----MISC-----
 local berserkTimer			= mod:NewBerserkTimer(600)
 mod:AddBoolOption("HealthFrame", true)
 mod:SetBossHealthInfo(
 	16064, L.Korthazz,
-	16062, L.Mograine,
+	30549, L.Rivendare,
 	16065, L.Blaumeux,
 	16063, L.Zeliek
 )
@@ -41,34 +32,6 @@ function mod:OnCombatStart(delay)
 	markCounter = 0
 end
 
-function mod:HolyWrath()
-	local target = nil
-	target = mod:GetBossTarget(16063)
-	local myName = UnitName("player")
-	if target == myName then
-		specWarnHolyWrathYou:Show()
-		SendChatMessage("Holy Wrath on"..UnitName("PLAYER").."!", "Say")
-	else
-		warnHolyWrath:Show(target)
-	end
-	timerHolyWrath:Start(target)
-	self:SetIcon(target, 1, 3)
-end
-
-function mod:DeepChill()
-	local target = nil
-	target = mod:GetBossTarget(16062)
-	local myName = UnitName("player")
-	if target == myName then
-		specWarnDeepChillYou:Show()
-		SendChatMessage("Deep Chill on"..UnitName("PLAYER").."!", "Say")
-	else
-		warnDeepChill:Show(target)
-	end
-	timerDeepChill:Start(target)
-	self:SetIcon(target, 6, 3)
-end
-
 local markSpam = 0
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(28832, 28833, 28834, 28835) and (GetTime() - markSpam) > 5 then
@@ -77,18 +40,9 @@ function mod:SPELL_CAST_SUCCESS(args)
 	end
 end
 
-function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(2124141) then
-		self:scheduleMethod(0.2, "HolyWrath")
-	end
-	if args:IsSpellID(2124167) then
-		self:scheduleMethod(0.2, "DeepChill")
-	end
-end
-
 function mod:SPELL_AURA_APPLIED_DOSE(args)
 	if args:IsSpellID(28832, 28833, 28834, 28835) and args:IsPlayer() then
-		if args.amount >= 3 then
+		if args.amount >= 4 then
 			specWarnMarkOnPlayer:Show(args.spellName, args.amount)
 		end
 	end
