3e59a5cf89d1b07a7697f5a9732c0baa424bba0a
Szyler <Szyler@Szyler.com>
2024-01-26 00:04:39 +0100

    modernizing and commenting out

--- DBM-BlackTemple/ShadeOfAkama.lua
+++ DBM-BlackTemple/ShadeOfAkama.lua
@@ -1,42 +1,42 @@
 local mod	= DBM:NewMod("Akama", "DBM-BlackTemple")
 local L		= mod:GetLocalizedStrings()
 
-Akama.Version		= "1.0"
-Akama.Author		= "Tandanu"
+mod:SetRevision(("$Revision: 5019 $"):sub(12, -3))
+mod:SetCreatureID(23421)
+mod:RegisterCombat("combat", 22841)
+-- Akama:SetMinCombatTime(60)
 
-local channelersDown = 0
-local sorcerersDown = 0
-
-Akama:SetCreatureID(23421)
-Akama:RegisterCombat("combat", 22841)
-Akama:SetMinCombatTime(60)
-
-Akama:RegisterEvents(
+mod:RegisterEvents(
 	"UNIT_DIED"
 )
 
-function Akama:OnCombatStart()
-	channelersDown = 0
-	sorcerersDown = 0
+function mod:OnCombatStart()
 end
 
-function Akama:OnEvent(event, arg1)
-	if event == "UNIT_DIED" then
-		if arg1.destName == DBM_AKAMA_MOB_CHANNELER then
-			self:SendSync("Channeler")
-		elseif arg1.destName == DBM_AKAMA_MOB_SORCERER then
-			self:SendSync("Sorcerer")
-		end
-	end
-end
 
-function Akama:OnSync(msg)
-	if msg == "Channeler" then
-		channelersDown = channelersDown + 1
-		self:Announce(DBM_AKAMA_WARN_CHANNELER_DOWN:format(channelersDown), 2)
-	elseif msg == "Sorcerer" then
-		sorcerersDown = sorcerersDown + 1
-		self:Announce(DBM_AKAMA_WARN_SORCERER_DOWN:format(sorcerersDown), 2)
-	end
-end
+-- local channelersDown = 0
+-- local sorcerersDown = 0
+-- channelersDown = 0
+-- sorcerersDown = 0
+
+
+-- function Akama:OnEvent(event, arg1)
+-- 	if event == "UNIT_DIED" then
+-- 		if arg1.destName == DBM_AKAMA_MOB_CHANNELER then
+-- 			self:SendSync("Channeler")
+-- 		elseif arg1.destName == DBM_AKAMA_MOB_SORCERER then
+-- 			self:SendSync("Sorcerer")
+-- 		end
+-- 	end
+-- end
+
+-- function Akama:OnSync(msg)
+-- 	if msg == "Channeler" then
+-- 		channelersDown = channelersDown + 1
+-- 		self:Announce(DBM_AKAMA_WARN_CHANNELER_DOWN:format(channelersDown), 2)
+-- 	elseif msg == "Sorcerer" then
+-- 		sorcerersDown = sorcerersDown + 1
+-- 		self:Announce(DBM_AKAMA_WARN_SORCERER_DOWN:format(sorcerersDown), 2)
+-- 	end
+-- end
 
