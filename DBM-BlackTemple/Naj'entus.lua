9cc77487e56b558f7d46a9bb06ed921ef87c0fa0
Szyler <Szyler@Szyler.com>
2024-05-21 23:37:37 +0200

    Adding targetTimer for discharge

--- DBM-BlackTemple/Naj'entus.lua
+++ DBM-BlackTemple/Naj'entus.lua
@@ -23,6 +23,7 @@ local warnPhase2			= mod:NewPhaseAnnounce(2)
 
 local timerNextShield		= mod:NewNextTimer(80, 2142521)
 local timerNextDischarge	= mod:NewNextTimer(20, 2142504)
+local timerDischarge		= mod:NewTargetTimer(8, 2142504)
 local timerTargetSpine		= mod:NewTargetTimer(30, 2142516)
 local timerNextSpine		= mod:NewNextTimer(30, 2142516)
 
@@ -59,6 +60,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		timerNextShield:Start()
 		timerNextSpine:Start(10)
 	elseif args:IsSpellID(2142504) then
+		timerDischarge:Start()
 		if args:IsPlayer() then
 			if self.Options.DischargeYellOpt then
 				SendChatMessage(L.SayDischargeFade, "SAY")
