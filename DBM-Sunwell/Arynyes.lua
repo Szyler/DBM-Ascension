aedf0e5c514d3f1771842ef7c8049337ca8136f7
strineX <87068357+strineX@users.noreply.github.com>
2024-09-08 21:09:16 +0200

    prepping files

--- DBM-Sunwell/Arynyes.lua
+++ DBM-Sunwell/Arynyes.lua
@@ -7,65 +7,52 @@ mod:SetUsedIcons()
 
 mod:RegisterCombat("combat")
 
-mod:RegisterEvents(
+mod:RegisterEventsInCombat(
 	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS",
+	"CHAT_MSG_MONSTER_YELL",
+	"CHAT_MSG_RAID_BOSS_YELL",
+	"CHAT_MSG_RAID_BOSS_EMOTE",
+	"CHAT_MSG_MONSTER_EMOTE",
+	"CHAT_MSG_RAID_WARNING",
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_APPLIED_DOSE",
 	"SPELL_AURA_REMOVED",
-	"SPELL_MISSED",
 	"UNIT_DIED"
 )
 
-local warnMeteor		= mod:NewSpellAnnounce(45150, 3)
-local warnBurn			= mod:NewTargetAnnounce(46394, 3)
-local warnStomp			= mod:NewTargetAnnounce(45185, 3)
-
-local timerMeteorCD		= mod:NewCDTimer(10, 45150)
-local timerBurn			= mod:NewTargetTimer(59, 46394)
 
 local berserkTimer		= mod:NewBerserkTimer(360)
 
 
 function mod:OnCombatStart(delay)
-
+	self.vb.phase = 1
 end
 
-function mod:OnCombatEnd()
-
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 46394 then
-		warnBurn:Show(args.destName)
-		
-	elseif args.spellId == 45185 then
-		warnStomp:Show(args.destName)
-	end
-end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
+function mod:SPELL_AURA_APPLIED(args)
+end
+
 function mod:SPELL_AURA_REMOVED(args)
-	if args.spellId == 46394 then
-		if self.Options.BurnIcon then
-			self:SetIcon(args.destName, 0)
-		end
-		timerBurn:Cancel(args.destName)
-	end
 end
 
 function mod:SPELL_CAST_START(args)
-	if args.spellId == 45150 then
-		warnMeteor:Show()
-		timerMeteorCD:Start()
-	end
-end
-
-function mod:SPELL_MISSED(args)
-	if args.spellId == 46394 then
-		warnBurn:Show("MISSED")
-	end
 end
 
 function mod:UNIT_DIED(args)
-	timerBurn:Cancel(args.destName)
+end
+
+
+
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, _, _, _, target)
+end
+mod.CHAT_MSG_RAID_BOSS_EMOTE = mod.CHAT_MSG_MONSTER_EMOTE
+mod.CHAT_MSG_RAID_BOSS_EMOTE = mod.CHAT_MSG_RAID_WARNING
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+end
+mod.CHAT_MSG_MONSTER_YELL = mod.CHAT_MSG_RAID_BOSS_YELL
+
+function mod:OnCombatEnd()
 end
