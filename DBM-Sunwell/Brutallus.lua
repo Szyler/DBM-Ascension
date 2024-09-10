3fee2c35737fc97acd881784de41f6b9424081cc
Szyler <66376500+Szyler@users.noreply.github.com>
2024-09-10 22:20:13 +0200

    Lua fix

--- DBM-Sunwell/Brutallus.lua
+++ DBM-Sunwell/Brutallus.lua
@@ -94,7 +94,7 @@ function mod:SPELL_DAMAGE(args)
 	if args:IsSpellID(2145719, 2145720, 2145721) and dbm:antiSpam(5) then
 		if args.destName == UnitName("Player") then
 			warnFelfireBurnYou:Show()
-		elseif
+		else
 			warnFelfireBurn:Show(args.destName)
 		end
 	end
