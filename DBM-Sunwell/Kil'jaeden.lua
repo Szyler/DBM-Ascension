e60d8c0a0dc5c566e9be6575de1f3dbe8b586b37
Szyler <66376500+Szyler@users.noreply.github.com>
2024-09-19 22:12:02 +0200

    First pass of Phase 1

--- DBM-Sunwell/Kil'jaeden.lua
+++ DBM-Sunwell/Kil'jaeden.lua
@@ -8,6 +8,7 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED_DOSE",
 	"SPELL_AURA_REMOVED",
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
@@ -15,65 +16,112 @@ mod:RegisterEvents(
 	"CHAT_MSG_MONSTER_EMOTE",
 	"CHAT_MSG_RAID_BOSS_EMOTE",
 	"CHAT_MSG_RAID_WARNING",
-	"SPELL_DAMAGE",
+	"CHAT_MSG_MONSTER_SAY",
 	"UNIT_DIED"
 )
 
 -- Apolyon
-local warnSoulbomb 				= mod:NewTargetAnnounce(2146682, 2)
-local YellSoulbomb 				= mod:NewFadesYell(2146682)
-local timerNextSoulbomb 		= mod:NewTargetTimer(10, 2146682)
+local warnSoulbomb 					= mod:NewTargetAnnounce(2146682, 2)
+local YellSoulbomb 					= mod:NewFadesYell(2146682)
+local timerNextSoulbomb 			= mod:NewNextTimer(10, 2146682)
 
-local warnSoulrend 				= mod:NewSpellAnnounce(2146680, 2)
-local timerNextSoulrend 		= mod:NewNextTimer(30, 2146680)
+local warnSoulrend 					= mod:NewSpellAnnounce(2146680, 2) -- 2146680 Spell_cast_Start
+local timerNextSoulRend 			= mod:NewNextTimer(30, 2146680) -- 2146680 Spell_cast_Start (20 yard radius)
+local timerCastSoulRend				= mod:NewCastTimer(3, 2146680) -- 2146680 Spell_cast_Start
 
--- Agamath
-local warnRagingBlow 			= mod:NewSpellAnnounce(2146684, 3)
-local timerNextRagingBlow 		= mod:NewNextTimer(10, 2146684)
+-- Agamath	
+-- local warnRagingBlow 				= mod:NewSpellAnnounce(2146684, 3)
+local timerNextRagingBlow 			= mod:NewNextTimer(10, 2146684)
 
-local warnFelRage 				= mod:NewTargetAnnounce(2146688, 2)
-local timerFelRage 				= mod:NewTargetTimer(5, 2146688)
+local warnFelRage 					= mod:NewTargetAnnounce(2146688, 2)
+local timerFelRage 					= mod:NewTargetTimer(5, 2146688)
 
--- Archonisus
-local warnSummonWildImps 		= mod:NewSpellAnnounce(2146676, 3)
-local timerNextSummonWildImps 	= mod:NewNextTimer(30, 2146676)
+-- Archonisus	
+local warnSummonWildImps 			= mod:NewSpellAnnounce(2146676, 3) -- 2146676 Spell_cast_Start
+local timerNextSummonWildImps 		= mod:NewNextTimer(30, 2146676) -- 2146676 Spell_cast_Start
+local timerCastImplosion 			= mod:NewCastTimer(30, 2146691)
+local warnImplosion 				= mod:NewSpellAnnounce(2146691, 3) -- Spell_Damage 2146691
 
--- local warnConflagration 		= mod:NewSpellAnnounce(2146673, 3)
-local timerConflagrationCast 	= mod:NewCastTimer(3, 2146673)
+-- local warnConflagration 			= mod:NewSpellAnnounce(2146673, 3)
+-- local timerConflagrationCast 		= mod:NewCastTimer(3, 2146673)
 
--- make warnings and timers for these, and make the trigger in their respective functions
--- Change all args.spellID into args:IsSpellID() with comma separated for the spellIDs
+-- make warnings and timers for 	these, and make the trigger in their respective functions
+-- Change all args.spellID into 	args:IsSpellID() with comma separated for the spellIDs
 
-local berserkTimer				= mod:NewBerserkTimer(900)
+local berserkTimer					= mod:NewBerserkTimer(900)
 
--- KJ timers
-local timerEmerge				= mod:NewTimer(17,"Kil'Jaeden is emerging")
+-- KJ timers	
+local timerEmerge					= mod:NewTimer(17,"Kil'Jaeden is emerging")
 
+local timerTargetLegionLightning	= mod:NewTargetTimer(4, 2146510) -- 2146510, 2146511 Spell_cast_start
+local warnTargetLegionLightning		= mod:NewTargetAnnounce(2146510, 2) -- 2146510, 2146511 Spell_cast_start
+local timerNextLegionLightning		= mod:NewNextTimer(20, 2146510) -- 2146510, 2146511 Spell_cast_start
+
+local timerCastAnnihilate			= mod:NewCastTimer(3, 2146557) -- 2146557 Spell_cast_start
+local warnAnnihilate				= mod:NewSpellAnnounce(2146557, 3) -- 2146557 Spell_cast_start
+local timeTargetTimerAnnihilate		= mod:NewTargetTimer(53, 2146557) -- 2146557 Spell_cast_start
+
+local timerNextWorldBreaker 		= mod:NewNextTimer(30, 2146520) -- 2146519, 2146520 Spell_cast_start
+local warnWorldBreaker				= mod:NewSpellAnnounce(2146520, 3) -- 2146519, 2146520 Spell_cast_start
+
+local timerCastAllConsuming	 		= mod:NewCastTimer(127, 2146521) -- 2146521 Spell_cast_start
+local timerNextAllConsuming	 		= mod:NewNextTimer(30, 2146521) -- 2146521 Spell_cast_start
+
+local timerNextDarkness				= mod:NewNextTimer(30, 2146540) -- 2146540, 2146541, 2146542 Spell_cast_start
+
+local timerNextReflections			= mod:NewNextTimer(25, 2146538) -- 2146522 Spell_cast_start
+
+local timerNextFireBloom			= mod:NewNextTimer(30, 2146523) -- 2146523, 2146524, 2146525, 2146526 Spell_cast_start
+local timerTargetFireBloom			= mod:NewTargetTimer(4, 2146523) -- 2146523, 2146524, 2146525, 2146526 Spell_cast_start
+
+
+self.vb.flamesDown = 0
 
 function mod:OnCombatStart(delay)
 	self.vb.phase = 1
+	self.vb.flamesDown = 0
 	timerNextSoulbomb:Start(5-delay)
 	timerNextSoulrend:Start(20-delay)
 end
 
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(2146682) then
-		warnSoulbomb:Show(args.destName)
-		timerNextSoulbomb:Start(args.destName)
-		YellSoulbomb:Countdown(10, 3)
-	elseif args:IsSpellID(2146680) then
+	if args:IsSpellID(2146680) then
 		warnSoulrend:Show()
 		timerNextSoulrend:Start()
+		timerCastSoulRend:Start()
 	elseif args:IsSpellID(2146684, 2146685) then
-		warnRagingBlow:Show()
+		-- warnRagingBlow:Show()
 		timerNextRagingBlow:Start()
 	elseif args:IsSpellID(2146673, 2146674, 2146675) then
 		-- warnConflagration:Show()
-		timerConflagrationCast:Start()
+		-- timerConflagrationCast:Start()
 	elseif args:IsSpellID(2146676) then
 		warnSummonWildImps:Show()
 		timerNextSummonWildImps:Start()
+		timerCastImplosion:Start()
+		warnImplosion:Schedule(27)
+	elseif args:IsSpellID(2146510, 2146511) then
+		warnTargetLegionLightning:Show(args.destName)
+		timerTargetLegionLightning:Start(args.destName)
+	elseif args:IsSpellID(2146557) then
+		timerCastAnnihilate:Start()
+		warnAnnihilate:Show()
+		timeTargetTimerAnnihilate:Start(args.sourceName)
+	elseif args:IsSpellID(2146520, 2146519) then
+		warnWorldBreaker:Show()
+		timerNextWorldBreaker:Start()
+	elseif args:IsSpellID(2146521) then
+		timerCastAllConsuming:Start()
+		timerNextAllConsuming:Start()
+	elseif args:IsSpellID(2146538) then
+		timerNextReflections:Start()
+	elseif args:IsSpellID(2146523, 2146524, 2146525, 2146526) then
+		timerNextFireBloom:Start()
+		timerTargetFireBloom:Start(args.destName)
+	elseif args:IsSpellID(2146540, 2146541, 2146542) then
+		timerNextDarkness:Start()
+
 	end
 end
 
@@ -81,6 +129,13 @@ function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(2146688) then
 		warnFelRage:Show(args.destName)
 		timerFelRage:Start(args.destName)
+	elseif args:IsSpellID(2146682) then
+		warnSoulbomb:Show(args.destName)
+		timerNextSoulbomb:Start(args.destName)
+		
+		if args.destName == UnitName("player") then
+			YellSoulbomb:Countdown(10, 3)
+		end
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
@@ -95,11 +150,32 @@ function mod:SPELL_CAST_SUCCESS(args)
 
 end
 
-function mod:CHAT_MSG_MONSTER_YELL(msg)
+function mod:CHAT_MSG_MONSTER_EMOTE(msg)
 	if msg == L.KJPull or msg:find(L.KJPull) then
 		self.vb.phase = 2
+		warnPhase2:Show()
+		timerEmerge:Start()
 	end
 end
+
+mod.CHAT_MSG_MONSTER_YELL = mod.CHAT_MSG_MONSTER_EMOTE
+mod.CHAT_MSG_RAID_BOSS_EMOTE = mod.CHAT_MSG_MONSTER_EMOTE
+mod.CHAT_MSG_MONSTER_SAY = mod.CHAT_MSG_MONSTER_EMOTE
+mod.CHAT_MSG_RAID_WARNING = mod.CHAT_MSG_MONSTER_EMOTE
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 25588 then -- 
+		timerNextSoulbomb:Stop()
+		timerNextSoulRend:Stop()
+	elseif cid == 25657 then -- Agamath
+		timerNextRagingBlow:Stop()
+		timerFelRage:Stop()
+	elseif cid == 25696 then -- Archonisus
+		timerNextSummonWildImps:Stop()
+	end
+end
+
 
 
 
